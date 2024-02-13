f308e2a725e6636500f8df74a11861c93677867e
Diego Almeida <diegoalmda@gmail.com>
2024-02-12 21:30:09 -0300

    feat: implement add new task context function

--- src/components/NewTaskInput/index.tsx
+++ src/components/NewTaskInput/index.tsx
@@ -1,6 +1,6 @@
 // React/React Native and expo imports
-import React, { useState } from 'react';
-import { type TextInputProps } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Alert, Keyboard, type TextInput, type TextInputProps } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 // External libs imports
@@ -8,15 +8,21 @@ import { useTheme } from 'styled-components';
 
 // Styles imports
 import { AddButton, Container, IconContainer, InputText } from './styles';
+import { useTaskContext } from '../../contexts/taskContext';
 
 interface Props extends TextInputProps {
   iconName: React.ComponentProps<typeof Feather>['name'];
   value?: string;
 }
 
-export function NewTaskInput({ iconName, value, ...rest }: Props): React.JSX.Element {
+export function NewTaskInput({ ...rest }: Props): React.JSX.Element {
   const [isFocused, setIsFocused] = useState(false);
   const [, setIsFilled] = useState(false);
+  const [taskTitle, setTaskTitle] = useState('');
+
+  const newTaskInputRef = useRef<TextInput>(null);
+
+  const { addNewTask } = useTaskContext();
 
   const theme = useTheme();
 
@@ -24,23 +30,39 @@ export function NewTaskInput({ iconName, value, ...rest }: Props): React.JSX.Ele
     setIsFocused(true);
   }
 
+  function addTask(title: string): void {
+    // Alert.alert(title);
+    addNewTask(title);
+    newTaskInputRef.current?.blur();
+    Keyboard.dismiss();
+    setTaskTitle('');
+  }
+
   function handleInputBlur(): void {
     setIsFocused(false);
-    setIsFilled(!!value);
+    setIsFilled(!!taskTitle);
   }
 
   return (
     <Container>
       <InputText
+        ref={newTaskInputRef}
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
         isFocused={isFocused}
+        value={taskTitle}
+        onChangeText={setTaskTitle}
         {...rest}
       />
 
       <IconContainer isFocused={isFocused}>
-        <AddButton activeOpacity={0.5}>
-          <Feather name={iconName} size={24} color={theme.colors.main} />
+        <AddButton
+          activeOpacity={0.5}
+          onPress={() => {
+            addTask(taskTitle);
+          }}
+        >
+          <Feather name="plus" size={24} color={theme.colors.main} />
         </AddButton>
       </IconContainer>
     </Container>
