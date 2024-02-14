bdabb59f5daecb3953bd3bfaa74a84fea7d6aa85
Diego Almeida <diegoalmda@gmail.com>
2024-02-14 00:36:48 -0300

    refactor: change keyboardavoidingview behavior

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -84,71 +84,72 @@ export function Home(): React.JSX.Element {
   }, []);
 
   return (
-    // <KeyboardAvoidingView
-    //   behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    //   keyboardVerticalOffset={20}
-    //   style={{ flex: 1 }}
-    // ></KeyboardAvoidingView>
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <Container>
-        <Header>
-          <LogoutButtonContainer onPress={signOut}>
-            <MaterialCommunityIcons name="logout" size={RFValue(28)} color={theme.colors.shape} />
-          </LogoutButtonContainer>
-          <AppHeroContainer>
-            <LogoContainer>
-              <Logo width={RFValue(36)} height={RFValue(36)} />
-              <Title>Lista de tarefas</Title>
-            </LogoContainer>
-            <SubTitle>Crie tarefas e organize seu dia</SubTitle>
-          </AppHeroContainer>
-          <TaskInputContainer>
-            <NewTaskInput />
-          </TaskInputContainer>
-        </Header>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={20}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <LogoutButtonContainer onPress={signOut}>
+              <MaterialCommunityIcons name="logout" size={RFValue(28)} color={theme.colors.shape} />
+            </LogoutButtonContainer>
+            <AppHeroContainer>
+              <LogoContainer>
+                <Logo width={RFValue(36)} height={RFValue(36)} />
+                <Title>Lista de tarefas</Title>
+              </LogoContainer>
+              <SubTitle>Crie tarefas e organize seu dia</SubTitle>
+            </AppHeroContainer>
+            <TaskInputContainer>
+              <NewTaskInput />
+            </TaskInputContainer>
+          </Header>
 
-        <Body>
-          <TaskCountersContainer>
-            <CounterContainer finished={false}>
-              <TasksInfo finished={false}>Total</TasksInfo>
-              <TasksCount finished={false}>{tasks.length}</TasksCount>
-            </CounterContainer>
-            <CounterContainer finished={true}>
-              <TasksInfo finished={true}>Concluídas</TasksInfo>
-              <TasksCount finished={true}>{totalTasksDone}</TasksCount>
-            </CounterContainer>
-          </TaskCountersContainer>
+          <Body>
+            <TaskCountersContainer>
+              <CounterContainer finished={false}>
+                <TasksInfo finished={false}>Total</TasksInfo>
+                <TasksCount finished={false}>{tasks.length}</TasksCount>
+              </CounterContainer>
+              <CounterContainer finished={true}>
+                <TasksInfo finished={true}>Concluídas</TasksInfo>
+                <TasksCount finished={true}>{totalTasksDone}</TasksCount>
+              </CounterContainer>
+            </TaskCountersContainer>
 
-          {tasks.length > 0 ? (
-            <TasksListContainer>
-              <FlatList
-                data={tasks}
-                keyExtractor={(item: Task) => item.id}
-                renderItem={({ item }) => (
-                  <Animated.View style={{ transform: [{ translateY: animatedValue }] }}>
-                    <TaskItem {...item} />
-                  </Animated.View>
-                )}
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{
-                  gap: 8,
-                  paddingBottom: 32,
-                }}
-              />
-            </TasksListContainer>
-          ) : (
-            <EmptyListContainer>
-              <FontAwesome5 name="tasks" size={RFValue(36)} color={theme.colors.text_light} />
-              <EmptyListMainMessage>Nenhuma tarefa cadastrada.</EmptyListMainMessage>
-              <EmptyListSecondaryMessage>
-                Entre com o título da tarefa e toque no
-                <EmptyListFeaturedTextIcon>“+”</EmptyListFeaturedTextIcon>
-                para cadastrar.
-              </EmptyListSecondaryMessage>
-            </EmptyListContainer>
-          )}
-        </Body>
-      </Container>
-    </TouchableWithoutFeedback>
+            {tasks.length > 0 ? (
+              <TasksListContainer>
+                <FlatList
+                  data={tasks}
+                  keyExtractor={(item: Task) => item.id}
+                  renderItem={({ item }) => (
+                    <Animated.View style={{ transform: [{ translateY: animatedValue }] }}>
+                      <TaskItem {...item} />
+                    </Animated.View>
+                  )}
+                  showsVerticalScrollIndicator={false}
+                  contentContainerStyle={{
+                    gap: 8,
+                    paddingBottom: 32,
+                  }}
+                />
+              </TasksListContainer>
+            ) : (
+              <EmptyListContainer>
+                <FontAwesome5 name="tasks" size={RFValue(36)} color={theme.colors.text_light} />
+                <EmptyListMainMessage>Nenhuma tarefa cadastrada.</EmptyListMainMessage>
+                <EmptyListSecondaryMessage>
+                  Entre com o título da tarefa e toque no
+                  <EmptyListFeaturedTextIcon>“+”</EmptyListFeaturedTextIcon>
+                  para cadastrar.
+                </EmptyListSecondaryMessage>
+              </EmptyListContainer>
+            )}
+          </Body>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
